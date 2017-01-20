e9b92a50874660e0c0195264cb26c60a4b48df84
kittu <kittu.enigma@gmail.com>
2017-01-20 12:04:47 -0800

    Add two numbers represented as singly linked list ( reverse repr i.e the units value is on the head node).

---
+++ practice-lc/src/prac/kp/medium/AddTwoNumbersReprAsLinkedLists.java
@@ -0,0 +1,46 @@
+package prac.kp.medium;
+
+/**
+ * 
+ * @author dkpriya
+ *
+ *<p><b>Problem Statement</b>
+ *You are given two non-empty linked lists representing two non-negative integers. 
+ *The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ *You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *<BR>Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ *<BR>Output: 7 -> 0 -> 8
+ *
+ *<p><b>Further clarifications:</b>
+ *<ul>  </ul>
+ *
+ *<p><b>Implementation strategies</b>
+ *<ul>
+ *</ul>
+ *
+ */
+public class AddTwoNumbersReprAsLinkedLists {
+
+	public static void main(String[] args) {
+ 
+
+	}
+	
+	public static ListNode addTwoNumbers(ListNode l1, ListNode l2 ){
+		
+ 		
+		return null;
+	}
+
+}
+
+/**
+ * 
+ * @author dkpriya
+ *  <p>Singly Linked Node representation class
+ */
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
